dddcf5cd9b3f0386db6545de0265133e812baa3c
caowanliang <1140482610@qq.com>
2022-09-05 15:49:07 +0800

    优化

--- TestCase/test_tian_chi_cloud/conftest.py
+++ TestCase/test_tian_chi_cloud/conftest.py
@@ -7,7 +7,6 @@
 import base64
 import pytest
 import allure
-from py.xml import html
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
@@ -15,8 +14,6 @@ from common.read_config import ini
 from common.read_element import Element
 from config.conf import cm
 from page_object.login_page import LoginPage
-from untils.send_mail import send_report
-from untils.times import timestamp
 
 driver = None
 
@@ -72,57 +69,6 @@ def pytest_runtest_makereport(item):
                        'onclick="window.open(this.src)" align="right"/></div>' % screen_img
                 extra.append(pytest_html.extras.html(html))
         report.extra = extra
-
-
-def pytest_html_results_table_header(cells):
-    cells.insert(1, html.th('用例名称'))
-    cells.insert(2, html.th('Test_node_id'))
-    cells.pop(2)
-
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(1, html.td(report.description))
-    cells.insert(2, html.td(report.nodeid))
-    cells.pop(2)
-
-
-def pytest_html_results_table_html(report, data):
-    if report.passed:
-        del data[:]
-        data.append(html.div('通过的用例未捕获日志输出.', class_='empty log'))
-
-
-def pytest_html_report_title(report):
-    report.title = "安恒超融合项目UI测试报告"
-
-
-def pytest_configure(config):
-    config._metadata.clear()
-    config._metadata['测试项目'] = "安恒超融合"
-    config._metadata['测试地址'] = ini.url
-
-
-def pytest_html_results_summary(prefix, summary, postfix):
-    # prefix.clear() # 清空summary中的内容
-    prefix.extend([html.p("所属部门: 安恒信息云产品线，测试部")])
-    prefix.extend([html.p("测试执行人: 电脑")])
-
-
-def pytest_terminal_summary(terminalreporter, exitstatus, config):
-    """收集测试结果"""
-    result = {
-        "total": terminalreporter._numcollected,
-        'passed': len(terminalreporter.stats.get('passed', [])),
-        'failed': len(terminalreporter  .stats.get('failed', [])),
-        'error': len(terminalreporter.stats.get('error', [])),
-        'skipped': len(terminalreporter.stats.get('skipped', [])),
-        # terminalreporter._sessionstarttime 会话开始时间
-        'total times': timestamp() - terminalreporter._sessionstarttime
-    }
-    print(result)
-    if result['failed'] or result['error']:
-        send_report()
-
 
 def _capture_screenshot():
     """截图保存为base64"""
